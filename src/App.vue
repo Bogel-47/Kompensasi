d7991f2690e726e5047b1ece7988ea7c9113a1eb
Hargi <hargimuhammad@gmail.com>
2022-06-22 12:44:42 +0700

    Add vue.js for kompen

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <!-- digunakan untuk melakukan input user -->
+  <input v-model="message" placeholder="edit me">
+  <!-- tempat untuk menampilkan inputan user -->
+  <p>Hallo {{ message }}</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+/* Style untuk website seperti font dll */
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
